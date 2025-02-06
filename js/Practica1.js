f763a31920275651e082eb6998e554fd8d448dd7
Álvaro López <lopezalvarezalvaro1@gmail.com>
2025-02-06 13:33:06 +0100

    feat: se añaden las figuras, falta ponerlas donde deberían estar (falla no sé porqué)

--- js/Practica1.js
+++ js/Practica1.js
@@ -1,21 +1,21 @@
 /**
+ *
  * Escena.js
- * 
+ *
  * Practica AGM #1. Escena basica en three.js
  * Seis objetos organizados en un grafo de escena con
  * transformaciones, animacion basica y modelos importados
- * 
- * @author 
- * 
+ *
+ * @author
+ *
  */
 
 // Modulos necesarios
-/*******************
- * TO DO: Cargar los modulos necesarios
- *******************/
+import * as THREE from '../lib/three.module.js'
+//import {GLTFLoader} from "../lib/GLTFLoader.module.js";
 
 // Variables de consenso
-let renderer, scene, camera;
+let renderer, scene, camera
 
 // Otras globales
 /*******************
@@ -23,61 +23,82 @@ let renderer, scene, camera;
  *******************/
 
 // Acciones
-init();
-loadScene();
-render();
+init()
+loadScene()
+render()
 
-function init()
-{
+function init() {
     // Motor de render
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    /*******************
-    * TO DO: Completar el motor de render y el canvas
-    *******************/
+    renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.getElementById('container').appendChild(renderer.domElement)
 
     // Escena
-    scene = new THREE.Scene();
-    
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(0.5, 0.5, 0.5)
+
     // Camara
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,1000);
-    camera.position.set( 0.5, 2, 7 );
-    camera.lookAt( new THREE.Vector3(0,1,0) );
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.set(0.5, 10, 7)
+    camera.lookAt(new THREE.Vector3(0, 1, 1))
 }
 
-function loadScene()
-{
-    const material = new THREE.MeshNormalMaterial( );
+function loadScene() {
+    const material = new THREE.MeshNormalMaterial()
+    const materialSuelo = new THREE.MeshBasicMaterial({ color: 'yellow', wireframe: true })
+    const materialGeom = new THREE.MeshBasicMaterial({ color: 'blue', wireframe: true })
 
-    /*******************
-    * TO DO: Construir un suelo en el plano XZ
-    *******************/
+    const suelo = new THREE.Mesh(new THREE.PlaneGeometry(10, 10, 10, 10), materialSuelo)
+    suelo.rotation.x = -Math.PI / 2
+    scene.add(suelo)
 
-    /*******************
-    * TO DO: Construir una escena con 5 figuras diferentes posicionadas
-    * en los cinco vertices de un pentagono regular alredor del origen
-    *******************/
+    const figuras = [
+        new THREE.RingGeometry(0.5, 1, 4),
+        new THREE.CylinderGeometry(0.5, 0.5, 1, 32),
+        new THREE.SphereGeometry(0.5, 20, 20),
+        new THREE.ConeGeometry(2, 0.5, 0.5),
+        new THREE.BoxGeometry(0.5, 0.5, 0.5),
+    ]
 
-    /*******************
-    * TO DO: Añadir a la escena un modelo importado en el centro del pentagono
-    *******************/
+    const radius = 5
+    const angulos = [0, Math.PI / 2.5, Math.PI, (3 * Math.PI) / 2.5, 2 * Math.PI]
 
+    const pentagonoGeom = new THREE.CircleGeometry(radius, 5)
+    const pentagon = new THREE.Mesh(pentagonoGeom, materialGeom)
+    pentagon.translateY = 0.5
+    pentagon.rotation.x = -Math.PI / 2
+    scene.add(pentagon)
+
+    for (let i = 0; i < 5; i++) {
+        const x = radius * Math.cos(angulos[i])
+        const z = radius * Math.sin(angulos[i])
+
+        const figura = new THREE.Mesh(figuras[i], material)
+        figura.position.set(x, 1, z)
+
+        scene.add(figura)
+    }
     /*******************
-    * TO DO: Añadir a la escena unos ejes
-    *******************/
+     * TO DO: Construir una escena con 5 figuras diferentes posicionadas
+     * en los cinco vertices de un pentagono regular alredor del origen
+     *******************/
+    /*******************
+     * TO DO: Añadir a la escena un modelo importado en el centro del pentagono
+     *******************/
+    /*******************
+     * TO DO: Añadir a la escena unos ejes
+     *******************/
 }
 
-function update()
-{
+function update() {
     /*******************
-    * TO DO: Modificar el angulo de giro de cada objeto sobre si mismo
-    * y del conjunto pentagonal sobre el objeto importado
-    *******************/
+     * TO DO: Modificar el angulo de giro de cada objeto sobre si mismo
+     * y del conjunto pentagonal sobre el objeto importado
+     *******************/
 }
 
-function render()
-{
-    requestAnimationFrame( render );
-    update();
-    renderer.render( scene, camera );
+function render() {
+    requestAnimationFrame(render)
+    update()
+    renderer.render(scene, camera)
 }
--- js/scene.js
+++ js/scene.js
@@ -1,102 +1,96 @@
-import * as THREE from "../lib/three.module.js";
-import {GLTFLoader} from "../lib/GLTFLoader.module.js";
+import * as THREE from '../lib/three.module.js'
+import { GLTFLoader } from '../lib/GLTFLoader.module.js'
 
 // Variables de consenso
-let renderer, scene, camera;
+let renderer, scene, camera
 
 // Otras globales
-let esferaCubo;
-let angulo = 0;
+let esferaCubo
+let angulo = 0
 
 // Acciones
-init();
-loadScene();
-render();
+init()
+loadScene()
+render()
 
-function init()
-{
+function init() {
     // Motor de render
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
+    renderer = new THREE.WebGLRenderer()
+    renderer.setSize(window.innerWidth, window.innerHeight)
     //renderer.setClearColor( new THREE.Color(0x0000AA) );
-    document.getElementById('container').appendChild( renderer.domElement );
+    document.getElementById('container').appendChild(renderer.domElement)
 
     // Escena
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0.5,0.5,0.5);
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(0.5, 0.5, 0.5)
 
     // Camara
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,1000);
-    camera.position.set( 0.5, 2, 7 );
-    camera.lookAt( new THREE.Vector3(0,1,0) );
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    camera.position.set(0.5, 2, 7)
+    camera.lookAt(new THREE.Vector3(0, 1, 0))
 }
 
-function loadScene()
-{
-    const material = new THREE.MeshBasicMaterial( { color: 'yellow', wireframe: true } );
+function loadScene() {
+    const material = new THREE.MeshBasicMaterial({ color: 'yellow', wireframe: true })
 
-    const geoCubo = new THREE.BoxGeometry( 2,2,2 );
-    const geoEsfera = new THREE.SphereGeometry( 1, 20,20 );
+    const geoCubo = new THREE.BoxGeometry(2, 2, 2)
+    const geoEsfera = new THREE.SphereGeometry(1, 20, 20)
 
-    const cubo = new THREE.Mesh( geoCubo, material );
-    const esfera = new THREE.Mesh( geoEsfera, material );
+    const cubo = new THREE.Mesh(geoCubo, material)
+    const esfera = new THREE.Mesh(geoEsfera, material)
 
     // Suelo
-    const suelo = new THREE.Mesh( new THREE.PlaneGeometry(10,10, 10,10), material );
-    suelo.rotation.x = -Math.PI / 2;
-    scene.add(suelo);
+    const suelo = new THREE.Mesh(new THREE.PlaneGeometry(10, 10, 10, 10), material)
+    suelo.rotation.x = -Math.PI / 2
+    scene.add(suelo)
 
     // Importar un modelo en json
-    const loader = new THREE.ObjectLoader();
+    const loader = new THREE.ObjectLoader()
 
-    loader.load( 'models/soldado/soldado.json', 
-        function(objeto){
-            cubo.add(objeto);
-            objeto.position.y = 1;
+    loader.load('models/soldado/soldado.json', function(objeto) {
+        cubo.add(objeto)
+        objeto.position.y = 1
+    })
+
+    // Importar un modelo en gltf
+    const glloader = new GLTFLoader()
+
+    glloader.load(
+        'models/RobotExpressive.glb',
+        function(gltf) {
+            //glloader.load( 'models/robota/scene.gltf', function ( gltf ) {
+            gltf.scene.position.y = 1
+            gltf.scene.rotation.y = -Math.PI / 2
+            esfera.add(gltf.scene)
+            console.log('ROBOT')
+            console.log(gltf)
+        },
+        undefined,
+        function(error) {
+            console.error(error)
         }
     )
 
-    // Importar un modelo en gltf
-    const glloader = new GLTFLoader();
+    esferaCubo = new THREE.Object3D()
+    esferaCubo.position.y = 1.5
+    cubo.position.x = -1
+    esfera.position.x = 1
+    cubo.add(new THREE.AxesHelper(1))
 
-    glloader.load( 'models/RobotExpressive.glb', function ( gltf ) {
-    //glloader.load( 'models/robota/scene.gltf', function ( gltf ) {
-        gltf.scene.position.y = 1;
-        gltf.scene.rotation.y = -Math.PI/2;
-        esfera.add( gltf.scene );
-        console.log("ROBOT");
-        console.log(gltf);
-    
-    }, undefined, function ( error ) {
-    
-        console.error( error );
-    
-    } );
+    scene.add(esferaCubo)
+    esferaCubo.add(cubo)
+    esferaCubo.add(esfera)
 
-    esferaCubo = new THREE.Object3D();
-    esferaCubo.position.y = 1.5;
-    cubo.position.x = -1;
-    esfera.position.x = 1;
-    cubo.add( new THREE.AxesHelper(1) );
-
-    scene.add( esferaCubo);
-    esferaCubo.add( cubo );
-    esferaCubo.add( esfera );
- 
-
-    scene.add( new THREE.AxesHelper(3) );
-
+    scene.add(new THREE.AxesHelper(3))
 }
 
-function update()
-{
-    angulo += 0.01;
-    esferaCubo.rotation.y = angulo;
+function update() {
+    angulo += 0.01
+    esferaCubo.rotation.y = angulo
 }
 
-function render()
-{
-    requestAnimationFrame( render );
-    update();
-    renderer.render( scene, camera );
+function render() {
+    requestAnimationFrame(render)
+    update()
+    renderer.render(scene, camera)
 }
