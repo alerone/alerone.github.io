b92d23093c111c48222f825bb6208cf29cf6ab74
Álvaro López <lopezalvarezalvaro1@gmail.com>
2025-02-07 13:29:09 +0100

    fix: camera angle and position

--- js/Practica1.js
+++ js/Practica1.js
@@ -45,7 +45,7 @@ function init() {
 
     // Camara
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    camera.position.set(0.5, 5, 4)
+    camera.position.set(0.5, 5, 7)
     camera.lookAt(new THREE.Vector3(0, 1, 1))
 }
 
